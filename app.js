ed0fa449eb278ff0a31920a5b854b32d88400757
LNWDARK <hnam@nextgensoft.co.th>
2018-09-07 01:33:35 +0700

    ss

--- app.js
+++ app.js
@@ -36,17 +36,19 @@ let app = new Vue({
         Success() {
             let richMenuId = 'richmenu-f971804c0d3036f376351d804fa582af'
             let userId = this.userId;
-            let channelAccessToken ='Bx004QWDFqOEh8K8RKfZlW5LVZNwdYvCGJK2AWNwhwjhKWvv8hmgBrOuiB/sXJNj1WQkn+0kwIxnEr0L8D8zseB70do4R/Pjs/UuxApg4lm728mSokfwgt1oncvUTcpwIML8wYGKv9j8SwylAixN9gdB04t89/1O/w1cDnyilFU='
+            let channelAccessToken = 'Bx004QWDFqOEh8K8RKfZlW5LVZNwdYvCGJK2AWNwhwjhKWvv8hmgBrOuiB/sXJNj1WQkn+0kwIxnEr0L8D8zseB70do4R/Pjs/UuxApg4lm728mSokfwgt1oncvUTcpwIML8wYGKv9j8SwylAixN9gdB04t89/1O/w1cDnyilFU='
             const url = `https://api.line.me/v2/bot/user/${userId}/richmenu/${richMenuId}`
-            // axios.post(url, {headers: {
-            //         Authorization: `Bearer ${channelAccessToken}`,
-            //     }})
-            //     .then(function (response) {
-            //         console.log(response);
-            //     })
-            //     .catch(function (error) {
-            //         console.log(error);
-            //     });
+            axios.post(`https://api.line.me/v2/bot/user/${userId}/richmenu/${richMenuId}`, {
+                    headers: {
+                        Authorization: `Bearer ${channelAccessToken}`,
+                    }
+                })
+                .then(function (response) {
+                    console.log(response);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
         }
     }
 
